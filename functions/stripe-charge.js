6e8cfd6b827728902d4407b0ddf95626ecebc43e
Zackaz <zackkuo2002@gmail.com>
2018-10-23 18:16:31 +0800

    20181023

---
+++ functions/stripe-charge.js
@@ -0,0 +1,24 @@
+const stripe = require('stripe')('sk_test_usJQW2FC8lE3DC6Sz1DDjaPl')
+
+exports.handler = async function (event) {
+    const {
+        tokenId,
+        email,
+        name,
+        description,
+        amount
+    } = JSON.parse(event.body)
+
+    const customer = await stripe.customers.create({
+        description: email,
+        source: tokenId
+    })
+
+    await stripe.charges.create({
+        customer: customer.id,
+        amount,
+        name,
+        description,
+        currency: 'usd'
+    })
+}
